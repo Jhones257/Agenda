853c5a5f2023d70227cbc795c480c2e023556230
Jhones de Sousa <mynamejhones@gmail.com>
2024-09-10 14:10:30 -0300

    Criando dados, terminar

--- contact/admin.py
+++ contact/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from contact.models import contact
+from contact import models
 
 # Register your models here.
 
-@admin.register(contact)
+@admin.register(models.contact)
 class contactAdmin(admin.ModelAdmin):
     list_display = ['id','frist_name', 'last_name', 'phone', 'email', 'created_at']
     search_fields = ['frist_name', 'last_name', 'phone', 'email']
@@ -14,3 +14,7 @@ class contactAdmin(admin.ModelAdmin):
     list_max_show_all = 100
     list_display_links = ['frist_name', 'last_name']
     
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['id']
--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=50)    
+    def __str__(self):
+        return self.name
+
 
 class contact(models.Model):
     frist_name = models.CharField(max_length=50)
@@ -11,6 +21,8 @@ class contact(models.Model):
     description = models.TextField()
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL,blank=True, null=True)
     
     def __str__(self):
         return self.frist_name + ' ' + self.last_name
