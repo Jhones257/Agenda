e9257e0ba44e162269d6be51fc313a63572adf8b
Jhones de Sousa <mynamejhones@gmail.com>
2024-09-09 21:42:06 -0300

    Criação Contact e do model ContactShow com banco de dados SQLite

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from contact.models import contact
 
 # Register your models here.
+
+@admin.register(contact)
+class contactAdmin(admin.ModelAdmin):
+    list_display = ['id','frist_name', 'last_name', 'phone', 'email', 'created_at']
+    search_fields = ['frist_name', 'last_name', 'phone', 'email']
+    list_filter = ['created_at']
+    date_hierarchy = 'created_at'
+    ordering = ['created_at']
+    list_per_page = 10
+    list_max_show_all = 100
+    list_display_links = ['frist_name', 'last_name']
+    
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class contact(models.Model):
+    frist_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    
+    def __str__(self):
+        return self.frist_name + ' ' + self.last_name
