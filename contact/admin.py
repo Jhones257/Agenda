eda54286ace8ff4880ff7d016cc753de0de03797
Jhones de Sousa <mynamejhones@gmail.com>
2024-09-11 15:32:33 -0300

    Criação do form de criação de contato

--- contact/admin.py
+++ contact/admin.py
@@ -5,15 +5,15 @@ from contact import models
 
 @admin.register(models.Contact)
 class contactAdmin(admin.ModelAdmin):
-    list_display = ['id','frist_name', 'last_name', 'phone', 'email', 'created_at', 'show']
-    search_fields = ['frist_name', 'last_name', 'phone', 'email']
+    list_display = ['id','first_name', 'last_name', 'phone', 'email', 'created_at', 'show']
+    search_fields = ['first_name', 'last_name', 'phone', 'email']
     list_filter = ['created_at']
     date_hierarchy = 'created_at'
     ordering = ['created_at']
     list_per_page = 10
     list_max_show_all = 100
     list_editable = ['show']
-    list_display_links = ['frist_name', 'last_name']
+    list_display_links = ['first_name', 'last_name']
     
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
--- contact/models.py
+++ contact/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 
 class Contact(models.Model):
-    frist_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
